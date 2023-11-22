684fd9060476f0c6334c2526302bcea682cda006
Gelena Bezusco <helenatcitok@gmail.com>
2023-11-22 16:06:25 +0000

    fixing file

--- src/channel.py
+++ src/channel.py
@@ -1,18 +1,19 @@
 from googleapiclient.discovery import build
+import os
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
-    def __init__(self, channel_id: str, api_key: str) -> None:
-        """Экземпляр инициализируется id канала и API-ключом."""
-        self.api_key = api_key
+    def __init__(self, channel_id: str) -> None:
+        """Экземпляр инициализируется id канала."""
+        self.api_key = os.getenv('YT_API_KEY')
         self.channel_id = channel_id
         self.name = None
         self.subscribers_count = None
 
-    def fetch_channel_info(self) -> None:
-        """Метод для загрузки информации о канале из API."""
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о канале."""
         youtube = build('youtube', 'v3', developerKey=self.api_key)
         channel = youtube.channels().list(
             id=self.channel_id, part='snippet,statistics'
@@ -23,11 +24,6 @@ class Channel:
             self.name = channel_data['snippet']['title']
             self.subscribers_count = channel_data[
                 'statistics']['subscriberCount']
-
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        if self.name is None or self.subscribers_count is None:
-            self.fetch_channel_info()
 
         print(f"Имя канала: {self.name}")
         print(f"Количество подписчиков: {self.subscribers_count}")
