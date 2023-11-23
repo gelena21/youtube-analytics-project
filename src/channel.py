e6f7dfca1a25bc957d40211ed493fa807f26c8e9
Gelena Bezusco <helenatcitok@gmail.com>
2023-11-23 10:14:32 +0000

    HW1

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,4 @@
+import json
 from googleapiclient.discovery import build
 import os
 
@@ -7,10 +8,9 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала."""
+        self.channel_id: str = channel_id
         self.api_key = os.getenv('YT_API_KEY')
-        self.channel_id = channel_id
-        self.name = None
-        self.subscribers_count = None
+        self.youtube = build("youtube", "v3", developerKey=self.api_key)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -18,12 +18,4 @@ class Channel:
         channel = youtube.channels().list(
             id=self.channel_id, part='snippet,statistics'
         ).execute()
-
-        if 'items' in channel and channel['items']:
-            channel_data = channel['items'][0]
-            self.name = channel_data['snippet']['title']
-            self.subscribers_count = channel_data[
-                'statistics']['subscriberCount']
-
-        print(f"Имя канала: {self.name}")
-        print(f"Количество подписчиков: {self.subscribers_count}")
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
